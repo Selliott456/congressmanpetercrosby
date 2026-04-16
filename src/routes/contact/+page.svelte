f006d79f8a0ea54a5c6930d18515030b99fee69d
Ell Peck <sherryllelliott@gmail.com>
2026-04-16 14:51:25 +0900

    adds email webhook

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,7 +3,7 @@
 	import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
 	import { messages } from '$lib/i18n/locale';
 
-	const EMAIL = 'petercrosbyforcongress@gmail.com';
+	const EMAIL = 'elliottpeck12@gmail.com';
 	const PHONE = '(801) 633-4297';
 	export let form;
 
